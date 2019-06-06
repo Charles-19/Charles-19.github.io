d7e1914e0ae50e0ca0833f5e78093e97e08c1447
Charles <upsilonalcubo@hotmail.com>
2019-06-06 04:14:50 -0500

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,32 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelector("#gallery-thumbs")
+                           .querySelectorAll("img");
+
+  // // EXERCISE
+  // let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+
+  let mainImage = document.querySelector("#gallery-photo > img");
+
+  thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      // // Works kind of the same, but it is not the best option
+      // mainImage.setAttribute("src", thumbnail.src);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+      // Change which image is current.
+      let currentClass = "current";
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
